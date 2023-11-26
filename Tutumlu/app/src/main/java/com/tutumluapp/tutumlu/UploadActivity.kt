4fe298e35a88b59e0cc854eb7253b70dd7c1bf48
Halil Efe Aköz <akoz20@itu.edu.tr>
2023-11-26 16:24:08 +0300

    Added upload screen UI with dynamic list creation without scanning functionality.

--- Tutumlu/app/src/main/java/com/tutumluapp/tutumlu/UploadActivity.kt
+++ Tutumlu/app/src/main/java/com/tutumluapp/tutumlu/UploadActivity.kt
@@ -1,11 +1,55 @@
 package com.tutumluapp.tutumlu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tutumluapp.tutumlu.databinding.ActivityUploadBinding
+import com.tutumluapp.tutumlu.databinding.ProductUploadFieldBinding
 
 class UploadActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityUploadBinding
+    private class Product(nameInput: String, barcodeInput: String, priceInput:Double, status: Int){
+        val name : String = nameInput
+        var barcode : String = barcodeInput
+        val price : Double = priceInput
+        var scanStatus : Int = status
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
+
+        binding.hdrUpload.ttHdr.text = "Upload Slip"
+        binding.hdrUpload.btnBack.setOnClickListener {
+            finish()
+        }
+        binding.hdrUpload.btnOptional.setImageResource(R.drawable.btn_profile)
+        binding.hdrUpload.btnOptional.setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
+
+        val customItems = arrayOf(
+            Product("Item 1", "", 2.5, 1),
+            Product("Item 2", "", 2.5, 1),
+            Product("Item 3", "", 2.5, 1)
+        )
+
+        customItems.forEach {product ->
+            binding.llProduct.addView(
+                ProductUploadFieldBinding.inflate(layoutInflater).also {listItem ->
+                    listItem.lblItemName.text = product.name
+                    listItem.lblScanInfo.text = "Scan barcode"
+                    listItem.lblPrice.text = product.price.toString()
+                    listItem.btnScan.setImageResource(R.drawable.logo_scan_black)
+                    listItem.btnScan.setOnClickListener {
+                        listItem.lblScanInfo.text = "Barcode canned!"
+                        listItem.btnScan.setImageResource(R.drawable.logo_tick)
+                        product.scanStatus = 0
+                    }
+                }.root
+            )
+        }
+
+
+        setContentView(binding.root)
     }
 }
