77257e71ae60a02c98f2ed5ef1e2147691d3b06f
Halil Efe Aköz <akoz20@itu.edu.tr>
2024-01-03 19:15:47 +0300

    Made experiments with imported supabase libraries

--- Tutumlu/app/src/main/java/com/tutumluapp/tutumlu/MainActivity.kt
+++ Tutumlu/app/src/main/java/com/tutumluapp/tutumlu/MainActivity.kt
@@ -3,8 +3,20 @@ package com.tutumluapp.tutumlu
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
 import com.tutumluapp.tutumlu.databinding.ActivityMainBinding
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.postgrest.postgrest
+import io.github.jan.supabase.postgrest.query.PostgrestResult
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,8 +24,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        val supabaseUrl = "https://qbpruczdytiwqouzoztl.supabase.co"
+        val supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFicHJ1Y3pkeXRpd3FvdXpvenRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcxMzE0ODUsImV4cCI6MjAxMjcwNzQ4NX0.aWqvr3-qOnJGQXZwk4GVNVXRnd2OKMOn3fkWF8Pm2UE"
 
-        binding.btnSignup.setOnClickListener {
+        val supabase = createSupabaseClient(supabaseUrl, supabaseKey) {
+            install(Postgrest)
+        }
+        @Serializable
+        data class Product(
+            @SerialName("price") val price: Double,
+            @SerialName("product_id") val barcode: Long
+        )
+
+        val products = supabase.postgrest.from("prices")
+
+        GlobalScope.launch {
+
+            val result = async {
+                //var i : PostgrestResult = products.insert(Product(17.5, 8690637939891))
+                /*var i : PostgrestResult = products.select(columns = Columns.list("gtin, name")) {
+                    filter(FilterOperation("name", FilterOperator.EQ,"%Algida%") )
+                }
+                Log.i("ErenTest",
+                    i.body?.jsonArray?.map { it.jsonObject["gtin"]?.jsonPrimitive?.long }.toString()
+                )*/
+
+                delay(2000)
+            }
+
+            val asyncResult = result.await()
+        }
+
+
+
+            binding.btnSignup.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
 
@@ -25,3 +69,4 @@ class MainActivity : AppCompatActivity() {
 
     }
 }
+
